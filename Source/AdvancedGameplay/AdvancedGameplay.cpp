2d90f88b2cd41fec3b9bd4d492d721cfcfb08280
Ossian Däckfors <ossian.dackfors@edu.futuregames.se>
2025-11-13 10:28:14 +0100

    init commit

---
+++ Source/AdvancedGameplay/AdvancedGameplay.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AdvancedGameplay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AdvancedGameplay, "AdvancedGameplay" );
